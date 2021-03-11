2d895192dce4cdcd60fbef0313dbcad8f0c89213
daluzi <1210415250@qq.com>
2021-03-11 09:28:28 +0800

    51

---
+++ 51.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2021/3/9 19:00
+@Author  : daluzi
+@File    : 51.py
+'''
+
+# 在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。在数组中的两个数字，如果前面一个数字大于后面的数字，则这两个数字组成一个逆序对。输入一个数组，求出这个数组中的逆序对的总数。
+class Solution:
+    def mergeSort(self, nums, tmp, l, r):
+        if l >= r:
+            return 0
+
+        mid = (l + r) // 2
+        inv_count = self.mergeSort(nums, tmp, l, mid) + self.mergeSort(nums, tmp, mid + 1, r)
+        i, j, pos = l, mid + 1, l
+        while i <= mid and j <= r:
+            if nums[i] <= nums[j]:
+                tmp[pos] = nums[i]
+                i += 1
+                inv_count += (j - (mid + 1))
+            else:
+                tmp[pos] = nums[j]
+                j += 1
+            pos += 1
+        for k in range(i, mid + 1):
+            tmp[pos] = nums[k]
+            inv_count += (j - (mid + 1))
+            pos += 1
+        for k in range(j, r + 1):
+            tmp[pos] = nums[k]
+            pos += 1
+        nums[l:r+1] = tmp[l:r+1]
+        return inv_count
+
+    def reversePairs(self, nums: List[int]) -> int:
+        n = len(nums)
+        tmp = [0] * n
+        return self.mergeSort(nums, tmp, 0, n - 1)
