8954c7c4b99b574d11a20b2461a092f4a195a2ba
daluzi <1210415250@qq.com>
2021-03-31 08:26:00 +0800

    52

---
+++ 52.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+'''
+@Time    : 2021/3/30 10:00
+@Author  : daluzi
+@File    : 52.py
+'''
+
+# 剑指 Offer 52. 两个链表的第一个公共节点
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> ListNode:
+        A, B = headA, headB
+        while A != B:
+            A = A.next if A else headB
+            B = B.next if B else headA
+        return A
