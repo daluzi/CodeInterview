01245dda2ef9139383ccfb6d40b324b3b8e1b64a
daluzi <1210415250@qq.com>
2021-04-29 16:01:13 +0800

    24

--- 24.py
+++ 24.py
@@ -5,6 +5,10 @@
 @File    : 24.py
 '''
 
+# 剑指 Offer 24. 反转链表
+# 定义一个函数，输入一个链表的头节点，反转该链表并输出反转后链表的头节点。
+
+# 迭代
 
 class Solution:
     def reverseList(self, head: ListNode) -> ListNode:
@@ -16,3 +20,14 @@ class Solution:
             currentNode = temp
 
         return preNode
+
+# 递归
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        if not head or not head.next:
+            return head
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return newHead
