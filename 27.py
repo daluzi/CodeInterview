93ec29676e9cec7db5291462ff62489e7a2a8ea8
daluzi <1210415250@qq.com>
2021-04-29 16:01:45 +0800

    27

--- 27.py
+++ 27.py
@@ -10,6 +10,15 @@
 
 class Solution:
     def mirrorTree(self, root: TreeNode) -> TreeNode:
+        # if not root: return
+        # root.left, root.right = self.mirrorTree(root.right), self.mirrorTree(root.left)
+        # return root
+
         if not root: return
-        root.left, root.right = self.mirrorTree(root.right), self.mirrorTree(root.left)
+        stack = [root]
+        while stack:
+            node = stack.pop()
+            if node.left: stack.append(node.left)
+            if node.right: stack.append(node.right)
+            node.left, node.right = node.right, node.left
         return root
